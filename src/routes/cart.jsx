2299397cc08e2dba0fbb6ce0b6facba46eae5557
Justin Lamb <justinlamb@me.com>
2024-05-08 10:56:58 -0700

    display items in cart

--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Navbar from "../components/nav";
-
+import "./cart.css";
 export default function Cart({ cart }) {
   const [items, setItems] = useState(cart);
 
@@ -8,10 +8,27 @@ export default function Cart({ cart }) {
     setItems([...items, product]);
   };
 
+  const cartSum = (items) => {
+    return items.reduce((sum, item) => sum + item.price, 0);
+  }
+
   return (
     <>
       <Navbar cart={cart} />
       <p>Cart</p>
+      <div className="cartContents">
+        {items.map((item) => (
+          <div key={item.id} className="cartItem">
+            <img src={item.image} alt={item.title} />
+            <p>{item.title}</p>
+            <p>${item.price.toFixed(2)}</p>
+          </div>
+        ))}
+        <div className="checkout">
+          <p>Total: ${cartSum(items).toFixed(2)}</p>
+          <button>Checkout</button>
+        </div>
+      </div>
     </>
   );
 }
