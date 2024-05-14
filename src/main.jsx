2da328013f48875420eddf43c7cc12f6caa48f6f
Justin Lamb <justinlamb@me.com>
2024-05-14 14:30:16 -0700

    app

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import App from './app.jsx';
+import App from './test.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<App />);
