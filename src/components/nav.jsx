30e6165953d03b21e3c6991ae694d67dc4f7ed38
Justin Lamb <justinlamb@me.com>
2024-05-14 14:00:31 -0700

    alert on checkout

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router-dom";
 
 export default function Navbar({ cart }) {
   let itemCount = cart.reduce((sum, item) => sum + item.quantity, 0);
-
+  const cartIcon = '\u{1F6D2}';
   return (
     <header>
       <h1>
         <Link to="/">Rio Grande</Link>
       </h1>
       <Link to="/cart">
-        <button id="cart">Cart ({itemCount})</button>
+        <button id="cart">{cartIcon} {itemCount}</button>
       </Link>
     </header>
   );
--- src/routes/cart.jsx
+++ src/routes/cart.jsx
@@ -1,8 +1,35 @@
 import Navbar from "../components/nav";
 import "./cart.css";
+
 export default function Cart({ cart, updateCart }) {
   const cartSum = (items) => {
-    return items.reduce((sum, item) => sum + (item.price * item.quantity), 0);
+    return items.reduce((sum, item) => sum + item.price * item.quantity, 0);
+  };
+
+  const checkoutAlert = () => {
+    const checkoutMsg = document.getElementById("checkoutMessage");
+    checkoutMsg.textContent = "Thank you for your purchase!";
+  };
+
+  const checkoutBuilder = () => {
+    if (cart.length === 0) {
+      return <p>Your cart is empty</p>;
+    } else {
+      return (
+        <>
+          <p>Total: ${cartSum(cart).toFixed(2)}</p>
+          <button
+            id="checkoutBtn"
+            onClick={() => {
+              checkoutAlert();
+            }}
+          >
+            Checkout
+          </button>
+          <p id="checkoutMessage"></p>
+        </>
+      );
+    }
   };
 
   return (
@@ -16,17 +43,21 @@ export default function Cart({ cart, updateCart }) {
             <div className="pricing">
               <p>Quantity: {item.quantity}</p>
               <div className="adjustButtons">
-                <button onClick={() => updateCart(item, "remove")}>-</button>
-                <button onClick={() => updateCart(item, "add")}>+</button>
+                <button
+                  className="remove"
+                  onClick={() => updateCart(item, "remove")}
+                >
+                  -
+                </button>
+                <button className="add" onClick={() => updateCart(item, "add")}>
+                  +
+                </button>
               </div>
               <p>${item.price.toFixed(2)} each</p>
             </div>
           </div>
         ))}
-        <div className="checkout">
-          <p>Total: ${cartSum(cart).toFixed(2)}</p>
-          <button>Checkout</button>
-        </div>
+        <div className="checkout">{checkoutBuilder()}</div>
       </div>
     </>
   );
